bffddb029a21b12735d16d35b63aa5e90d5df5a2
Adrien Heisch <adrienheisch@orange.fr>
2024-12-07 01:25:41 +0100

    better lexer code

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -2,7 +2,7 @@ module Lexer (Lexer.lex) where
 
 import CharClasses qualified as CC
 import Cursor (Cursor (Cursor))
-import Cursor qualified (end, expand, idx, len)
+import Cursor qualified (end, expand)
 import Data.Text as Text (Text)
 import Data.Text qualified as Text (drop, index, length, take, unpack)
 import Identifier (Id (Id))
@@ -11,15 +11,15 @@ import Token (Token)
 import Token qualified
 
 lex :: Text -> [Token]
-lex text = reduceTokens (lexFrom text 0)
+lex text = reduceTokens $ lexFrom text 0
 
 reduceTokens :: [Token] -> [Token]
-reduceTokens tokens = case tokens of
-  [] -> []
-  (Token.NL : Token.NL : rest) -> reduceTokens (Token.NL : rest)
-  (Token.Op Op.Lt : Token.Id (Id str) : Token.Op Op.MemberPtr : Token.Id (Id str') : Token.Op Op.Gt : rest) ->
-    reduceTokens (Token.ImplInclude (str ++ "." ++ str') : rest)
-  (tk : rest) -> tk : reduceTokens rest
+reduceTokens = foldr go []
+  where
+    go Token.NL (Token.NL : rest) = Token.NL : rest
+    go (Token.Op Op.Lt) (Token.Id (Id name) : Token.Op Op.MemberPtr : Token.Id (Id ext) : Token.Op Op.Gt : rest) =
+      Token.ImplInclude (name ++ "." ++ ext) : rest
+    go tk rest = tk : rest
 
 lexFrom :: Text -> Int -> [Token]
 lexFrom text from = case takeToken text from of
@@ -28,65 +28,57 @@ lexFrom text from = case takeToken text from of
   (token, from') -> token : lexFrom text from'
 
 takeToken :: Text -> Int -> (Token, Int)
-takeToken text from =
-  if from >= Text.length text
-    then (Token.Eof, from)
-    else do
-      let cursor = getCursor text (Text.index text from) from
-      case readCursor text cursor of
-        "//" -> (Token.Nil, skipLine text (Cursor.end cursor))
-        "/*" -> (Token.Nil, skipBlock text (Cursor.end cursor))
-        str -> (Token.make str, Cursor.end cursor)
+takeToken text from
+  | from >= Text.length text = (Token.Eof, from)
+  | otherwise =
+      let cursor = getCursor text from
+       in case readCursor text cursor of
+            "//" -> (Token.Nil, skipLine text (Cursor.end cursor))
+            "/*" -> (Token.Nil, skipBlock text (Cursor.end cursor))
+            str -> (Token.make str, Cursor.end cursor)
 
-getCursor :: Text -> Char -> Int -> Cursor
-getCursor text first idx = do
-  let crsr = makeCursor idx
-  case first of
-    _ | first `elem` CC.identifierStart -> getIdent crsr
-    _ | first `elem` CC.digits -> getNumLit crsr
-    _ | first `elem` CC.punctuators -> getPunct crsr
-    '"' -> getStrLit crsr
-    '\'' -> getCharLit crsr
-    ' ' -> getWhite crsr
-    '\n' -> getNewLine crsr
-    _ -> crsr
+getCursor :: Text -> Int -> Cursor
+getCursor text idx = go (Cursor idx 1)
   where
-    makeCursor start_idx = Cursor start_idx 1
+    go cursor
+      | first == ' ' = getWhile cursor (== ' ')
+      | first == '\n' = getWhile cursor (== '\n')
+      | first == '"' = getUntil cursor '"'
+      | first == '\'' = getUntil cursor '\''
+      | first `elem` CC.identifierStart = getWhile cursor $ \c -> c `elem` CC.identifier
+      | first `elem` CC.digits = getWhile cursor $ \c -> c `elem` CC.float
+      | first `elem` CC.punctuators = getWhile cursor $ \c -> c `elem` CC.punctuators
+      | otherwise = cursor
+      
+    first =  Text.index text idx
+
     peek cursor = Text.index text (Cursor.end cursor)
-    getIdent cursor = case peek cursor of
-      c | c `elem` CC.identifier -> getIdent $ Cursor.expand cursor
-      _ -> cursor
-    getNumLit cursor = case peek cursor of
-      c | c `elem` CC.float -> getNumLit $ Cursor.expand cursor
-      _ -> cursor
-    getPunct cursor = case peek cursor of
-      c | c `elem` CC.punctuators && Cursor.len cursor < 2 -> getPunct $ Cursor.expand cursor
-      _ -> cursor
-    getStrLit cursor = case peek cursor of
-      c | c == '"' -> Cursor.expand cursor
-      _ -> getStrLit $ Cursor.expand cursor
-    getCharLit cursor = case peek cursor of
-      '\'' -> Cursor.expand cursor
-      '\\' -> getCharLit $ Cursor.expand $ Cursor.expand cursor
-      _ -> getCharLit $ Cursor.expand cursor
-    getWhite cursor = case peek cursor of
-      c | c == ' ' -> getWhite $ Cursor.expand cursor
-      _ -> cursor
-    getNewLine cursor = case peek cursor of
-      c | c == '\n' -> getNewLine $ Cursor.expand cursor
-      _ -> cursor
+
+    getUntil cursor terminator =
+      if peek cursor == terminator
+        then Cursor.expand cursor
+        else getUntil (Cursor.expand cursor) terminator
+
+    getWhile cursor predicate =
+      if predicate (peek cursor)
+        then getWhile (Cursor.expand cursor) predicate
+        else cursor
 
 readCursor :: Text -> Cursor -> String
-readCursor text cursor = Text.unpack (Text.take (Cursor.len cursor) (Text.drop (Cursor.idx cursor) text))
+readCursor text (Cursor idx len) = Text.unpack $ Text.take len $ Text.drop idx text
 
 skipLine :: Text -> Int -> Int
-skipLine text idx =
-  if idx >= Text.length text || Text.index text idx == '\n'
-    then idx
-    else skipLine text (idx + 1)
+skipLine text = go
+  where
+    go idx
+      | idx >= len || Text.index text idx == '\n' = idx
+      | otherwise = go (idx + 1)
+    len = Text.length text
 
 skipBlock :: Text -> Int -> Int
-skipBlock text idx =
-  if idx + 1 >= Text.length text || readCursor text (Cursor idx 2) == "*/"
-    then idx + 2
-    else skipBlock text (idx + 1)
+skipBlock text = go
+  where
+    go idx
+      | idx + 1 >= len || readCursor text (Cursor idx 2) == "*/" = idx + 2
+      | otherwise = skipBlock text (idx + 1)
+    len = Text.length text
