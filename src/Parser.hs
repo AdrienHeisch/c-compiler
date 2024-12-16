9da02817797b1586ae4e4b7cec4de3ce356bc49b
Adrien Heisch <adrienheisch@orange.fr>
2024-12-16 04:03:02 +0100

    fix while statements

--- src/Parser.hs
+++ src/Parser.hs
@@ -328,13 +328,9 @@ case_ taken = do
 
 while :: [Token] -> State [Token] Statement
 while taken = do
-  tokens <- get
-  case map Token.def tokens of
-    TD.DelimOpen Dl.Pr : _ -> do
-      modify $ drop 1
-      cond <- collectUntilDelimiter Dl.Pr
-      body <- statement
-      return $ Statement (SD.While (expr cond) body) (taken ++ cond ++ St.tks body)
+  cond <- collectUntilDelimiter Dl.Pr
+  body <- statement
+  return $ Statement (SD.While (expr cond) body) (taken ++ cond ++ St.tks body)
     _ -> return $ Statement (SD.Invalid "Expected (") taken
 
 doWhile :: [Token] -> State [Token] Statement
