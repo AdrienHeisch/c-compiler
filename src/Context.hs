79ec1eac2a23a3aece3ad43acf3d80eb20ced7bc
Adrien Heisch <adrienheisch@orange.fr>
2024-12-18 00:09:57 +0100

    simpler Context type

--- src/Context.hs
+++ src/Context.hs
@@ -4,36 +4,35 @@ import Control.Monad.State.Lazy (State, get, put)
 import Data.List (find)
 import Identifier (Id)
 import Type (Type)
-import Type qualified (len)
+import Instruction (regLen)
 
 type Var = (Int, Type, Id)
 
 data Context = Context
   { vars :: [Var],
-    nvar :: Int,
     nlbl :: Int,
     prev :: Maybe Context
   }
   deriving (Show)
 
 new :: Maybe Context -> Context
-new = Context [] 0 0
+new = Context [] 0
 
 addVars :: [(Type, Id)] -> Context -> Context
 addVars vars context = foldl (flip addVar) context vars
 
 addVar :: (Type, Id) -> Context -> Context
-addVar (ty, name) (Context vars nvar lbls prev) = Context (vars ++ [(nvar, ty, name)]) (nvar + Type.len ty) lbls prev
+addVar (ty, name) (Context vars nlbl prev) = Context (vars ++ [(length vars * regLen, ty, name)]) nlbl prev
 
 getVar :: Context -> Id -> Maybe (Int, Type)
-getVar (Context vars _ _ _) name = case find (byId name) vars of
+getVar (Context vars _ _) name = case find (byId name) vars of
   Nothing -> Nothing
   Just (idx, ty, _) -> Just (idx, ty)
 
 getLabel :: State Context Int
 getLabel = do
-  Context vars nvar nlbl prev <- get
-  put $ Context vars nvar (nlbl + 1) prev
+  Context vars nlbl prev <- get
+  put $ Context vars (nlbl + 1) prev
   return nlbl
 
 byId :: Id -> Var -> Bool
