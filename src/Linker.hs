37a623c11130e678b6541c469e90debce73e22a1
Adrien Heisch <adrienheisch@orange.fr>
2024-12-22 19:02:08 +0100

    replace labels in all instructions

--- src/Linker.hs
+++ src/Linker.hs
@@ -45,8 +45,31 @@ secondPass ins = case ins of
 
 replaceLabels :: Instruction -> AsmState Instruction
 replaceLabels instr = case instr of
+  HALT (Lbl lbl) -> mk HALT lbl
+  SYCALL (Lbl lbl) -> mk SYCALL lbl
   SET r (Lbl lbl) -> mk (SET r) lbl
+  LOAD r (Lbl lbl) -> mk (LOAD r) lbl
+  STORE r (Lbl lbl) -> mk (STORE r) lbl
+  CMP r (Lbl lbl) -> mk (CMP r) lbl
+  ADD r (Lbl lbl) -> mk (ADD r) lbl
+  SUB r (Lbl lbl) -> mk (SUB r) lbl
+  MUL r (Lbl lbl) -> mk (MUL r) lbl
+  DIV r (Lbl lbl) -> mk (DIV r) lbl
+  MOD r (Lbl lbl) -> mk (MOD r) lbl
+  AND r (Lbl lbl) -> mk (AND r) lbl
+  OR r (Lbl lbl) -> mk (OR r) lbl
+  XOR r (Lbl lbl) -> mk (XOR r) lbl
+  NAND r (Lbl lbl) -> mk (NAND r) lbl
+  NOR r (Lbl lbl) -> mk (NOR r) lbl
+  NXOR r (Lbl lbl) -> mk (NXOR r) lbl
+  SHL r (Lbl lbl) -> mk (SHL r) lbl
+  SHR r (Lbl lbl) -> mk (SHR r) lbl
+  RCL r (Lbl lbl) -> mk (RCL r) lbl
+  RCR r (Lbl lbl) -> mk (RCR r) lbl
+  PUSH (Lbl lbl) -> mk PUSH lbl
+  DUP (Lbl lbl) -> mk DUP lbl
   CALL (Lbl lbl) -> mk CALL lbl
+  RET (Lbl lbl) -> mk RET lbl
   JMP (Lbl lbl) -> mk JMP lbl
   JEQ r (Lbl lbl) -> mk (JEQ r) lbl
   JNE r (Lbl lbl) -> mk (JNE r) lbl
@@ -54,6 +77,8 @@ replaceLabels instr = case instr of
   JGE r (Lbl lbl) -> mk (JGE r) lbl
   JLT r (Lbl lbl) -> mk (JLT r) lbl
   JLE r (Lbl lbl) -> mk (JLE r) lbl
+  PRINT (Lbl lbl) -> mk PRINT lbl
+  EPRINT (Lbl lbl) -> mk EPRINT lbl
   LABEL _ -> error "Label instructions should not appear at this point"
   _ -> return instr
   where
