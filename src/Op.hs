269e11b550f7dd5a3f976d464428bff5d5ab3d10
Adrien Heisch <adrienheisch@orange.fr>
2024-12-29 00:45:33 +0100

    fix associativity of member operators

--- src/Op.hs
+++ src/Op.hs
@@ -143,9 +143,7 @@ rightAssociative :: [Op]
 rightAssociative =
   binaryAssign
     ++ ternary
-    ++ [ Assign,
-         Member,
-         MemberPtr
+    ++ [ Assign
        ]
 
 isRightAssociative :: Op -> Bool
